3d62f89050cf4e97a3fb440ab168b1c8ae386211
Bruno <brunocesaragweb@gmail.com>
2019-05-10 11:17:16 -0300

    Atualização para inserir link para a área lab no menu

--- bg-app/src/app/menu/menu.module.ts
+++ bg-app/src/app/menu/menu.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu.component';
 
 @NgModule({
     declarations: [
         MenuComponent
+    ],
+    imports:[
+        RouterModule
     ],
     exports: [
         MenuComponent
