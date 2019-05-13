98a73376905696229d639e4d1c6d4811ce34cb95
Bruno Cesar Aparecido Gaudino <brunogaudino@brq.com>
2019-05-13 11:38:27 -0300

    Atualização área aboutme, e atualização de informações SEO

--- polyfills.js
+++ polyfills.js
@@ -5496,8 +5496,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/brunogaudino/local-repositories/brunogaudino.github.io/bg-app/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /home/brunogaudino/local-repositories/brunogaudino.github.io/bg-app/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\www\brunogaudino.github.io\bg-app\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\www\brunogaudino.github.io\bg-app\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
