6feac2100970ec1dd45882373a31761d415ee2c1
Bruno Cesar Aparecido Gaudino <brunogaudino@brq.com>
2019-05-24 17:03:10 -0300

    Nova área do site resume com o curriculo

--- bg-app-material/src/app/menu/menu.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'bg-menu',
-    templateUrl: './menu.component.html',
-    styleUrls: ['./menu.component.css']
-})
-
-export class MenuComponent { }
--- bg-app/src/app/content/resume/resume.component.ts
+++ bg-app/src/app/content/resume/resume.component.ts
@@ -5,4 +5,4 @@ import { Component } from "@angular/core";
     styleUrls: ['./resume.component.css']
 })
   
-export class ResumeComponent {}  
+export class ResumeComponent {}
