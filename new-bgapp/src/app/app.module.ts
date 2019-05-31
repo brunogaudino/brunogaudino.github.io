03fdb36dbddc466ffbc512769d677e53ab76d285
Bruno Cesar Aparecido Gaudino <brunogaudino@brq.com>
2019-05-31 18:26:11 -0300

    Iniciando novo layout do site

--- new-bgapp/src/app/app.module.ts
+++ new-bgapp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContentModule } from './content/content.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ContentModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {}
+}
