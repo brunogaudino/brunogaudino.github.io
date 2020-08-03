32e08291d8e554a72b5ae5633933bbaa80309276
Bruno <brunocesaragweb@gmail.com>
2020-08-03 16:38:13 -0300

    Update in About section, home, menu, social network, global, layout changes

--- bootstrap-app/src/app/content/about/about.component.ts
+++ bootstrap-app/src/app/content/about/about.component.ts
@@ -3,12 +3,14 @@ import { environment } from '../../../environments/environment.prod';
 
 @Component({
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
   jobPosition: string = environment.jobPosition;
   college: string = environment.college;
+  employer: string = environment.employer;
+  employerWebSite: string = environment.employerWebSite;
 
   constructor() { }
 
