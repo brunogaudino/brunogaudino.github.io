f8d354fb05c276fb029c191b87f377cbabd2f389
Bruno C. A. Gaudino <brunocesaragweb@gmail.com>
2023-08-30 09:28:02 -0300

    update social icons build prod structure

--- develop/bootstrap-app/src/app/shared/mock/social-network.mock.ts
+++ develop/bootstrap-app/src/app/shared/mock/social-network.mock.ts
@@ -2,43 +2,49 @@ export const SocialNetworkMock = [
     {
         'title': 'Linkedin',
         'link': 'https://linkedin.com/in/brunocesarweb/?locale=en_US',
-        'icon': 'fa-linkedin',
+        'icon': 'linkedin',
         'flagShowHide': true
     },
     {
         'title': 'GitHub',
         'link': 'https://github.com/brunogaudino',
-        'icon': 'fa-github',
+        'icon': 'github',
         'flagShowHide': true
     },
     {
         'title': 'Linktree',
         'link': 'https://linktr.ee/brunocagaudino',
-        'icon': 'fa-tree',
+        'icon': 'linktree',
         'flagShowHide': true
     },
     {
         'title': 'Medium',
         'link': 'https://medium.com/@brunogaudino',
-        'icon': 'fa-medium',
+        'icon': 'medium',
         'flagShowHide': true
     },
     {
         'title': 'Stackblitz',
         'link': 'https://stackblitz.com/@brunogaudino',
-        'icon': 'fa-code',
+        'icon': 'stackblitz',
         'flagShowHide': false
     },
     {
         'title': 'Behance',
         'link': 'https://www.behance.net/brunocesarweb',
-        'icon': 'fa-behance',
+        'icon': 'behance',
         'flagShowHide': false
     },
     {
         'title': 'CodePen',
         'link': 'http://codepen.io/brunocesaragweb/',
-        'icon': 'fa-codepen',
+        'icon': 'codepen',
+        'flagShowHide': false
+    },
+    {
+        'title': 'DEV.to',
+        'link': 'http://dev.to/brunogaudino/',
+        'icon': 'devdotto',
         'flagShowHide': false
     }
 ]
