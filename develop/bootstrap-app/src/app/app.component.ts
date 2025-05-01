0de94bcb647da97c2a8d872b967548b86b51623b
Bruno <brunocesaragweb@gmail.com>
2025-05-01 10:48:37 -0300

    gh-pages create

--- develop/bootstrap-app/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-//import * as $ from 'jquery';
-
-@Component({
-  selector: 'bg-root',
-  template: '<router-outlet></router-outlet>'
-})
-export class AppComponent { }
--- develop/bootstrap-app/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  jobPosition: 'Systems analyst front-end',
-  college: 'Technician\'s license',
-  employer: 'WBA Software',
-  employerWebSite:'https://wba.com.br/'
-};
